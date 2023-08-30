a97b9b1361a915767e2c2ec3afbf5a601ef44eb0
Operachi <81317803+Operachi061@users.noreply.github.com>
2023-08-30 12:43:44 +0000

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -18,7 +18,7 @@ if (Quest == "Yes"):
     print("2")
     time.sleep(1)
     print("1")
-    os.system("git clone https://github.com/HeheYolo/FastOS-For-Fun-Not-Real.git")
+    os.system("git clone https://github.com/Operachi061/FastOS-For-Fun-Not-Real.git")
     shutil.move("./FastOS-For-Fun-Not-Real/.git", "./TrashFilesToDelete")
     shutil.move("./FastOS-For-Fun-Not-Real/.gitignore", "./TrashFilesToDelete")
     shutil.move("./FastOS-For-Fun-Not-Real/README.md", "./TrashFilesToDelete")
@@ -28,19 +28,17 @@ if (Quest == "Yes"):
     os.rmdir("./FastOS-For-Fun-Not-Real/src")
     os.rmdir("./FastOS-For-Fun-Not-Real/")
     print("Compiling: FastOS-Shell")
-    if os.system("g++ ./src/FastOS-Shell.cpp") == 0:
+    if os.system("g++ ./src/FastOS-Shell.cpp -o FastOS-Shell") == 0:
         print("Done!")
     else:
         print ("Error")
-    os.rename(r"./a.exe",r"./FastOS-Shell.exe" )
     print("Compiling: FastOS-Bootloader")
-    if os.system("g++ ./src/FastOS-Bootloader.cpp") == 0:
+    if os.system("g++ ./src/FastOS-Bootloader.cpp -o FastOS-Bootloader") == 0:
         print("Done!")
     else:
         print ("Error")
-    os.rename(r"./a.exe",r"./FastOS-Bootloader.exe" )
     print("Finish!")
-    print("Start FastOS by typing: ./FastOS-Bootloader.exe or in linux/macos ./FastOS-Bootloader")
+    print("Start FastOS by typing: ./FastOS-Bootloader")
 
 if (Quest == "No"):
     print("Canceled")
@@ -50,7 +48,7 @@ if (Quest == "yes"):
     print("2")
     time.sleep(1)
     print("1")
-    os.system("git clone https://github.com/HeheYolo/FastOS-For-Fun-Not-Real.git")
+    os.system("git clone https://github.com/Operachi061/FastOS-For-Fun-Not-Real.git")
     shutil.move("./FastOS-For-Fun-Not-Real/.git", "./TrashFilesToDelete")
     shutil.move("./FastOS-For-Fun-Not-Real/.gitignore", "./TrashFilesToDelete")
     shutil.move("./FastOS-For-Fun-Not-Real/README.md", "./TrashFilesToDelete")
@@ -60,18 +58,16 @@ if (Quest == "yes"):
     os.rmdir("./FastOS-For-Fun-Not-Real/src")
     os.rmdir("./FastOS-For-Fun-Not-Real/")
     print("Compiling: FastOS-Shell")
-    if os.system("g++ ./src/FastOS-Shell.cpp") == 0:
+    if os.system("g++ ./src/FastOS-Shell.cpp -o FastOS-Shell") == 0:
         print("Done!")
     else:
         print ("Error")
-    os.rename(r"./a.exe",r"./FastOS-Shell.exe" )
     print("Compiling: FastOS-Bootloader")
-    if os.system("g++ ./src/FastOS-Bootloader.cpp") == 0:
+    if os.system("g++ ./src/FastOS-Bootloader.cpp -o FastOS-Bootloader") == 0:
         print("Done!")
     else:
         print ("Error")
-    os.rename(r"./a.exe",r"./FastOS-Bootloader.exe" )
     print("Finish!")
-    print("Start FastOS by typing: ./FastOS-Bootloader.exe or in linux/macos ./FastOS-Bootloader")
+    print("Start FastOS by typing: ./FastOS-Bootloader")
 if (Quest == "no"):
     print("Canceled")
