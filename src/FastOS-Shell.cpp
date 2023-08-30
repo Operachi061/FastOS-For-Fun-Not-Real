0414857e821fd2f43c450d0352ac635e1252c97b
Operachi <81317803+Operachi061@users.noreply.github.com>
2023-08-30 12:46:34 +0000

    Update FastOS-Shell.cpp

--- src/FastOS-Shell.cpp
+++ src/FastOS-Shell.cpp
@@ -1,3 +1,54 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
+
+int Turbo;
+string commandline;
+string computer;
+string name;
+
+int main()
+{
+   cout << "Your name: ";
+   cin >> name;
+   cout << "Computer name: ";
+   cin >> computer;
+    //Turbo - Package manager
+    //Turbo-make-install
+    //Turbo-make-delete
+    //Turbo-make-sync
+
+    //Command line and simple commands
+while (true)
+{
+      cout << computer << "@" << name << ":";
+      cin >> commandline;
+       if (commandline == "turbo-make-help")
+       {
+          cout << "delete - remove/unistall package" << endl;
+          cout << "install - install package" << endl;
+          cout << "sync - sync/update packages" << endl;
+       }
+       else cout << "Error" << endl;
+       if (commandline == "turbo-make-install")
+       {
+          cout << "delete - remove/unistall package" << endl;
+       }
+       else
+       {
+          cout << "Error" << endl;
+       }
+       if (commandline == commandline)
+       {
+         //cout << computer << "@" << name << ":";
+         cin >> commandline;
+       }
+       if (commandline == "exit")
+       {
+         exit(0);
+       } else cout << "Error" << endl;
+}
+
+  return 0;
+}
